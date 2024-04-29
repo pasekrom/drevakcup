5b606f02e060f4907593114cf668d946fc33852e
pas <roman.pasek@pchelp.cz>
2024-04-29 13:15:02 +0200

    Password reset

--- core/urls.py
+++ core/urls.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from core import settings, views
-from core.views import Home, User, Cups, SignUpView, MyLoginView
+from core.views import Home, UserProfile, Cups, SignUpView, MyLoginView, forgot_password
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('iihf/', include('iihf.urls')),
     path('', Home.as_view(), name='home'),
-    path('user/', User.as_view(), name='user'),
+    path('user/', UserProfile.as_view(), name='user'),
     path('cups/', Cups.as_view(), name='cups'),
     path('', Home.as_view(), name='sign-out'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', MyLoginView.as_view(), name='login'),
     path('logout/', views.logout_page, name='logout'),
+    path('forgot-password/', forgot_password, name='forgot_password'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ] \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
+from django.contrib import messages
 from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, PasswordResetView
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import views as auth_views
+from core.models import User
 
 from core.forms import CustomUserCreationForm
 from iihf.models import Cup
@@ -22,7 +25,7 @@ class Home(TemplateView):
         return context
 
 
-class User(LoginRequiredMixin, TemplateView):
+class UserProfile(LoginRequiredMixin, TemplateView):
     template_name = 'core/user.html'
 
 
@@ -54,3 +57,18 @@ def logout_page(request):
     logout(request)
     return redirect('/')
 
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if User.objects.filter(email=email).exists():
+            # Directly call PasswordResetView to trigger the password reset process
+            return PasswordResetView.as_view(
+                email_template_name='core/password_reset_email.html',
+                subject_template_name='core/password_reset_subject.txt',
+                success_url=reverse('password_reset_done')
+            )(request)
+        else:
+            messages.error(request, 'User with this email does not exist.')
+            return redirect('forgot_password')
+    return render(request, 'core/forgot_password.html')
