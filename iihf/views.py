7f99fde3ff14d92a03723a1a80f957e13dfa6441
pas <roman.pasek@pchelp.cz>
2024-05-26 22:01:45 +0200

    Update views.py

--- iihf/views.py
+++ iihf/views.py
@@ -358,13 +358,22 @@ def calculate_points(request, year):
 
             if special.winner == special_tips.winner:
                 user_point_b.points += 24
+            # Check for final_a matches and add points
             if special.final_a == special_tips.final_a or special.final_a == special_tips.final_b:
                 user_point_b.points += 16
-            if special.final_b == special_tips.final_b or special.final_b == special_tips.final_a and special_tips.final_a != special_tips.final_b:
+
+            # Check for final_b matches and add points, ensuring correct logic with parentheses
+            if (
+                    special.final_b == special_tips.final_b or special.final_b == special_tips.final_a) and special_tips.final_a != special_tips.final_b:
                 user_point_b.points += 16
+
+            # Check for bronze_a matches and add points
             if special.bronze_a == special_tips.bronze_a or special.bronze_a == special_tips.bronze_b:
                 user_point_b.points += 12
-            if special.bronze_b == special_tips.bronze_b or special.bronze_b == special_tips.bronze_a and special_tips.bronze_a != special_tips.bronze_b:
+
+            # Check for bronze_b matches and add points, ensuring correct logic with parentheses
+            if (
+                    special.bronze_b == special_tips.bronze_b or special.bronze_b == special_tips.bronze_a) and special_tips.bronze_a != special_tips.bronze_b:
                 user_point_b.points += 12
             if special.czech_shooter_first == special_tips.czech_shooter_first:
                 user_point_b.points += 12
